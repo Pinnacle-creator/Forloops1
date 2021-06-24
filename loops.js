fbfce222dfe9c45b66b8b1afccac1bbe1e6b4526
Pinnacle-creator <joerir@gmail.com>
2021-06-24 16:59:26 +0200

    loops

--- loops.js
+++ loops.js
@@ -6,7 +6,10 @@
 // Hoera!
 // ==========================================
 
-
+const shout = "Hoera!"
+for (let i = 0; i < 3; i++) {
+    console.log(shout);
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -16,10 +19,18 @@
 // loop...
 // loop...
 // klaar!
- // ==========================================
+// ==========================================
 
+const word = "loop...";
+const word1 = "klaar!";
 
-
+for (let i = 0; i < 5; i++) {
+    if (i === 4) {
+        console.log(word1);
+    } else {
+        console.log(word);
+    }
+}
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
 // Verwachte uitkomsten:
@@ -33,6 +44,15 @@
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // ==========================================
 
+for (let i = 0; i < 15; i++) {
+    if (i % 2) {
+        console.log(i + " is oneven")
+    } else {
+        console.log(i + " is even")
+    }
+
+
+}
 
 
 // ==========================================
